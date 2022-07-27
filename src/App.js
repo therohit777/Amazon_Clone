53a320d183ab009b50e8cf5c54efd15b6572aeaa
Rohit Pandey <rohitpandeyofficial777@gmail.com>
2022-07-27 19:50:16 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 import { Header } from './Components/Js/Header';
 import { Home } from './Components/Js/Home';
+import { BrowserRouter as Router , Route, Routes } from 'react-router-dom';
+import { Checkoutpage } from './Components/Js/Checkoutpage';
+
+
 
 function App() {
   return (
     <div className="App">
-     <Header/>
-     <Home/>
+      <Router>
+         <Header/>
+         <Routes>
+           <Route path="/checkout" element={<Checkoutpage/>}/>
+           <Route path="/" element={<Home/>}/>
+         </Routes>
+       </Router>
     </div>
   );
 }
---
+++ src/Components/Js/Checkoutpage.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import "../Css/checkout.css";
+import Checkout_banner from "../Images/checkoutbanner.jpg";
+import { Subtotal } from './Subtotal';
+
+export const Checkoutpage = () => {
+  return (
+    <div className='checkout'>
+        <div className="checkout_left">
+          <img src={Checkout_banner} className="check_ad" alt="None" />
+        <div>
+          <h2 className="checkout_title">Your Shopping Basket</h2>
+        </div>
+        </div>
+
+
+        <div className="checkout_right">
+          <Subtotal/>       
+        </div>
+
+    </div>
+  )
+}
