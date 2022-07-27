271d5ddf0ac4beceda0b30bcb44e06379975459c
Rohit Pandey <rohitpandeyofficial777@gmail.com>
2022-07-27 23:12:13 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import { Header } from './Components/Js/Header';
 import { Home } from './Components/Js/Home';
-import { BrowserRouter as Router , Route, Routes } from 'react-router-dom';
 import { Checkoutpage1 } from './Components/Js/Checkoutpage1';
 
 
@@ -9,13 +8,9 @@ import { Checkoutpage1 } from './Components/Js/Checkoutpage1';
 function App() {
   return (
     <div className="App">
-      <Router>
-         <Header/>
-         <Routes>
-           <Route path="/checkout" element={<Checkoutpage1/> }/>
-           <Route path="/" element={<Home/>}/>
-         </Routes>
-       </Router>
+      <Header/>
+      <Home/>
+      <Checkoutpage1/>
     </div>
   );
 }
--- src/Components/Js/Header.js
+++ src/Components/Js/Header.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import "../Css/header.css";
 import { MdSearch,MdShoppingBasket } from "react-icons/md";
-import { Link } from 'react-router-dom';
+
 
 export const Header = () => {
   return (
     <div className='header'>
-        <Link to="/">
+        
         <img src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" className="headerlogo"alt="" />
-        </Link>
+        
         <div className="searchcompo">
             <input type="text" className='headersearch_box'/>
              <MdSearch className='header_SearchIcon' />
@@ -27,12 +27,12 @@ export const Header = () => {
                 <span className='opt_line2'>Prime</span>
             </div>
 
-            <Link to="/checkout">
+            
                 <div className="nav_optionbasket">
                     <MdShoppingBasket className='headerbasketicon'/>
                     <span className='opt_line2 nav_basketcount'>0</span>            
                 </div>
-            </Link>
+            
         </div>
     </div>
   )
